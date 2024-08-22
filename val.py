dfdbcdc76d714180d4e14274bddda5586b625f95
VincentYuuuuuu <105576506+VincentYuuuuuu@users.noreply.github.com>
2024-08-22 15:44:58 +0800

    Update val.py

--- val.py
+++ val.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     model = YOLO('Weights Here')
     model.val(data='Data Yaml Path',
                 split='val',
-                save_json=False, # if you need to cal coco metrice
+                save_json=False, # if you need to cal coco metrics
                 project='runs/val',
                 name='exp',
                 device='',
